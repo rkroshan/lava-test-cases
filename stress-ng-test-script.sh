1b1ae20d8cb6b9ddc749eca8e96bb40483902456
rkroshan <rkroshan.1999@gmail.com>
2020-05-13 22:37:25 +0530

    stress-ng testscript extra features v21

--- stress-ng-test-script.sh
+++ stress-ng-test-script.sh
@@ -1,25 +1,50 @@
 #!/bin/sh
 
+
+export args_list="-C -F --all --params"
+export run_all_tests=0
+export terminal_test=""
+export file_names=""
+export params_list="--perf --tz --metrics-brief -v"
+export time_to_run=30
+export cpu_instance=0
+
+export   all_tests="crypt \
+					cpu-online \
+					hdd \
+					matrix \
+					seek \
+					sock \
+					pipe \
+					mq \
+					bsearch \
+					lsearch"
+
+
 ####basic stress-ng testing
 
 . ./lib/sh-test-lib
 echo "About to run stress-ng-test ..."
 
 #Command: -C := if passing testcases via terminal
-#Command: -F := if passing testcases from a file
+#Command: -F := if passing testcases from a file    
 
-#Usage: ./stress-ng-test-script.sh -C testcase1 testcase2 ...
-#Usage: ./stress-ng-test-script.sh -F filename1 filename2 ...
+#Usage: ./stress-ng-test-script.sh -C testcase1 testcase2 ... -F filename1 filename2 ... --all --params param1 param2 param3 .. --time 30 --cpu_instance 1
 usage_func() {
-    echo "Usage: ${0} -C testcase1 testcase2 ...		#if passing testcases via terminal " 
-    echo "Usage: ${0} -F filename1 filename2 ...		#if passing testcases from a file" 
+    echo "Usage: ${0} [ -C "testcase1 testcase2 ..." ]
+				    [ -F "filename1 filename2 ..." ]
+				    [ --all 0]
+				    [ --params "param1 param2 param3 .." ]
+				    [ --time 30 ]
+				    [ --cpu_instance 0 ]	" 
+    
     exit 0
 }
 
 
 run_stress_ng_test(){
 	echo "Running $1 Test"
-	stress-ng --$1 0 -t 30 --perf --tz --metrics-brief -v 2>&1 | tee result.txt 
+	stress-ng --$1 $cpu_instance -t $time_to_run $params_list 2>&1 | tee result.txt 
 	VAR=$(grep -c " successful " result.txt )
 	lava_test_result_stress_ng "$1-test" $VAR
 	unset VAR
@@ -27,22 +52,61 @@ run_stress_ng_test(){
 }
 
 stress_ng_test_cases_terminal(){
-	shift
-	for i in "$@"
+	for i in $@
 	do
 	run_stress_ng_test $i
 	done
 }
 
 stress_ng_test_cases_file(){
-	shift
-	for i in "$@"
+	for i in $file_names
 	do
 		filename=$i
 		while read line; do
-			echo $line
+			#echo $line
 			run_stress_ng_test $line
 		done < $filename
+	done
+}
+
+
+
+parse_argument(){
+
+	while [ -n "$1" ] 
+	do
+		#echo $1
+		case $1 in
+			-C)	
+				shift
+				export terminal_test="$1" 
+				shift ;;
+
+			-F)
+				shift
+				export file_names="$1" 
+				shift ;;
+			--params)
+				shift
+				export params_list="$1" 
+				shift ;;
+			--time)
+				shift
+				export time_to_run=$1 
+				shift ;;
+			--cpu_instance)
+				shift
+				export cpu_instance=$1 
+				shift ;;
+			--all)
+				shift
+				export run_all_tests=$1 
+				shift ;;
+			*) 
+				echo "Invalid Arguments"
+				usage_func
+				;;
+		 esac
 	done
 }
 
@@ -53,14 +117,20 @@ main_function(){
 	args="$@"
 	Command_arg=$1
 
-	if [ "${Command_arg}" = "-C" ]; then
-		stress_ng_test_cases_terminal "$@"
-	elif [ "${Command_arg}" = "-F" ]; then
-		stress_ng_test_cases_file "$@"
+	parse_argument "$@"
+
+	if [ "${run_all_tests}" -eq 1 ]; then 
+		#echo $all_tests
+		stress_ng_test_cases_terminal $all_tests
 	else
-		echo "wrong arguments"
-		usage_func
+		if [ -n terminal_test ]; then
+			stress_ng_test_cases_terminal $terminal_test
+		fi
+		if [ -n file_names ]; then
+			stress_ng_test_cases_file
+		fi
 	fi
+	
 }
 
 #Running the script

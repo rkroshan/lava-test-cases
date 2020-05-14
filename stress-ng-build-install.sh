4f3af32d49cdcd8512b641b61b939f6f021ff5fc
rkroshan <rkroshan.1999@gmail.com>
2020-05-14 13:13:12 +0530

    stress-ng Test-Script new feature v33

--- stress-ng-build-install.sh
+++ stress-ng-build-install.sh
@@ -2,19 +2,19 @@
 
 # compile and install stress-ng version 0.11.09
 	. ./lib/sh-test-lib
-	
+
     echo "started installing stress-ng version 0.11.09 ..."
     echo "installing dependencies ..."
     install_deps "git build-essential libaio-dev libapparmor-dev libattr1-dev libbsd-dev libcap-dev libipsec-mb-dev libjudy-dev libkeyutils-dev libsctp-dev zlib1g-dev"
     echo "cloning git://kernel.ubuntu.com/cking/stress-ng.git ... "
     git clone git://kernel.ubuntu.com/cking/stress-ng.git
-    (
-        cd stress-ng || echo "stress-ng folder not found." && exit
-        # Use 0.11.09 release for building.
-        git checkout V0.11.09 -b V0.11.09
-        echo "building and installing stress-ng ..."
-        make clean && make && make install
-    )
+    
+    cd stress-ng || error_msg "stress-ng folder not found." 
+    # Use 0.11.09 release for building.
+    git checkout V0.11.09 -b V0.11.09
+    echo "building and installing stress-ng ..."
+    make clean && make && make install
+   
 
     if which stress-ng; then
         echo "stress-ng version: $(stress-ng --version)"
